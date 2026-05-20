fe530407f79a9d18b680b694d9e76fe0e36a8321
samwynsma <cpcs.samuel.wynsma@gmail.com>
2026-05-20 13:39:17 -0700

    Implement menu of connect four game.
    Add the menu for the connect four game.

---
+++ GamesList/ConnectFourBoard.java
@@ -0,0 +1,10 @@
+package GamesList;
+
+public class ConnectFourBoard {
+
+    public void DropIntoColumn(String columnString) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'DropIntoColumn'");
+    }
+
+}
--- GamesList/ConnectFourGame.java
+++ GamesList/ConnectFourGame.java
@@ -1,9 +1,33 @@
 package GamesList;
+import java.util.Scanner;
+
 public class ConnectFourGame {
 
     public void PlayGame() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'PlayGame'");
+        System.out.println("Welcome to Connect 4. In this game, the goal is to get four elements in a row.");
+        boolean isGameOver = false;
+        boolean isPlayerOne = true;
+        Scanner columnScanner = new Scanner(System.in);
+        ConnectFourBoard board = new ConnectFourBoard();
+        while(!isGameOver)
+        {
+            if(isPlayerOne)
+            {
+                System.out.println("Please select a column to drop your tile into, player 1.");
+            }
+            else
+            {
+                System.out.println("Please select a column to drop your tile into, player 2.");
+            }
+            String columnString = columnScanner.nextLine().toLowerCase();
+            if(columnString == "quit")
+            {
+                break;
+            }
+            board.DropIntoColumn(columnString);
+        }
+
+        columnScanner.close();
     }
 
 }
