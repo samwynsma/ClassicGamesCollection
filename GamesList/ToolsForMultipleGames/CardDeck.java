2e9be173670fdeb5053fecdcae89abdd2fb73206
samwynsma <cpcs.samuel.wynsma@gmail.com>
2026-05-26 14:58:58 -0700

    Add two constructors to deck.
    One constructor is for a standard deck with thirteen card suits, the other is for variable decks with variable card counts.

--- GamesList/ToolsForMultipleGames/CardDeck.java
+++ GamesList/ToolsForMultipleGames/CardDeck.java
@@ -2,12 +2,34 @@ package GamesList.ToolsForMultipleGames;
 
 public class CardDeck {
 
+    String[] cards;
     public CardDeck(char[] suits) {
-        //TODO Auto-generated constructor stub
+        String cardVals = "23456789TJQKA";
+        cards = new String[suits.length * cardVals.length()];
+        for(int i = 0; i < suits.length; i++)
+        {
+            for(int j = 0; j < 13; j++)
+            {
+                StringBuilder card = new StringBuilder();
+                card.append(suits[i]);
+                card.append(cardVals.charAt(j));
+                cards[i * 13 + j] = card.toString();
+            }
+        }
     }
 
     public CardDeck(char[] suits, int cardCount) {
-
+        cards = new String[suits.length * cardCount];
+        for(int i = 0; i < suits.length; i++)
+        {
+            for(int j = 0; j < cardCount; j++)
+            {
+                StringBuilder card = new StringBuilder();
+                card.append(suits[i]);
+                card.append(j+1);
+                cards[i * cardCount + j] = card.toString();
+            }
+        }
     }
     
 }
