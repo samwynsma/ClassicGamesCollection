0eea498a635420de60da069b00fa9c30daf9b725
samwynsma <cpcs.samuel.wynsma@gmail.com>
2026-06-03 14:50:53 -0700

    Adds basic information to risky dice game.

--- GamesList/RiskyDiceGame.java
+++ GamesList/RiskyDiceGame.java
@@ -12,7 +12,24 @@ public class RiskyDiceGame {
         System.out.println("Sometimes, you gain the ability to move onto the next level of dice. This means that the rewards get higher, but so does the risk. You'll be given the choice to proceed if you wish.");
         int players = 0;
         String playString = "";
+        while(players < 2 || players > 10)
+        {
+            System.out.println("How many players will be playing risky dice? Please enter a number between 2 and 10.");
+            playString = menuPrompt.nextLine().toLowerCase();
+            try
+            {
+                players = Integer.parseInt(playString);
+            }
+            catch(Exception e)
+            {
+                System.out.println("Please enter a number between 2 and 10");
+            }
+        }
         RiskyDiceGameController gameInfo = new RiskyDiceGameController(players);
+        while(!playString.equals("quit") && !gameInfo.IsGameOver())
+        {
+            System.out.println("Do you want to continue, player " + gameInfo.GetCurrentPlayer() + "? Type roll to continue or pass to stop. Type quit to end the game early.");
+        }
     }
     
 }
--- GamesList/RiskyDiceMaterials/RiskyDiceGameController.java
+++ GamesList/RiskyDiceMaterials/RiskyDiceGameController.java
@@ -2,8 +2,23 @@ package GamesList.RiskyDiceMaterials;
 
 public class RiskyDiceGameController {
 
+    private int[] playerScores;
+    private int players;
+    private int currentPlayer;
+
     public RiskyDiceGameController(int players) {
-        //TODO Auto-generated constructor stub
+        this.players = players;
+        this.playerScores = new int[players];
+        this.currentPlayer = 1;
+    }
+
+    public boolean IsGameOver()
+    {
+        return currentPlayer > players;
+    }
+
+    public int GetCurrentPlayer() {
+        return currentPlayer;
     }
     
 }
