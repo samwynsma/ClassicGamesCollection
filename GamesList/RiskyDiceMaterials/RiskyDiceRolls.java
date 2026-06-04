285e0efea66c2782bbbd39ea5f5417438e094e4a
samwynsma <cpcs.samuel.wynsma@gmail.com>
2026-06-04 14:08:36 -0700

    Added third die to dice rolls.

--- GamesList/RiskyDiceMaterials/RiskyDiceRolls.java
+++ GamesList/RiskyDiceMaterials/RiskyDiceRolls.java
@@ -122,7 +122,53 @@ public class RiskyDiceRolls {
     }
 
     private int DieThree(){
-        return 0;
+        int rollValue = diceSet[1].RollDice();
+        int score = 0;
+        Random rand = new Random();
+        switch(rollValue){
+            case 1 -> {
+                isLoss = true;
+                System.out.println("Oh dear. Looks like you rolled a \"you lose\" roll. That means you lose.");
+            }
+            case 2 -> {
+                score = rand.nextInt(10) + 5;
+                System.out.println("You rolled a small gain. That means that you gained " + score + " points.");
+            }
+            case 3 -> {
+                score = rand.nextInt(15) + 20;
+                System.out.println("You rolled a medium gain. That means that you gained " + score + " points.");
+            }
+            case 4 -> {
+                score = 2;
+                divider = true;
+                System.out.println("Oh no, you rolled a divider! Your score will be cut in half. :(");
+            }
+            case 5 -> {
+                score = 2;
+                multiplier = true;
+                System.out.println("Nice, you rolled a multiplier! Your score will be doubled! :)");
+            }
+            case 6 -> {
+                score = - (rand.nextInt(10) + 5);
+                System.out.println("You rolled a small loss. That means that you lost " + score + " points.");
+            }
+            case 7 -> {
+                score = 100;
+                System.out.println("You got the tier 2 jackpot! You scored 100 points!");
+            }
+            case 8 -> {
+                score = - (rand.nextInt(15) + 20);
+                System.out.println("You rolled a medium loss. That means that you lost " + score + " points.");
+            }
+            case 9 -> {
+                System.out.println("You rolled a nothing. That means that nothing happens.");
+            }
+            case 10 -> {
+                canAdvance = true;
+                System.out.println("You rolled an advance. That means that you can advance to the next die whenever you want.");
+            }
+        }
+        return score;
     }
 
     private int DieFour(){
