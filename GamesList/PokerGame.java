ecea3e7042a71aa40348e478a2334d359aa1695b
samwynsma <cpcs.samuel.wynsma@gmail.com>
2026-07-01 15:33:57 -0700

    Adds initial player count for poker game.

--- GamesList/PokerGame.java
+++ GamesList/PokerGame.java
@@ -8,6 +8,32 @@ public class PokerGame {
         System.out.println("Welcome to the game of Poker.");
         System.out.println("This version of Poker is played with up to six players.");
         System.out.println("A player can get rid of up to five cards from their hand, then they will compare hands with everyone else.");
+        System.out.println("Game will go until you choose to quit or say no when asked to play again.");
+        String playString = "";
+        int players = 0;
+
+        while(players < 2 || players > 10)
+        {
+            System.out.println("How many players do you want to have in this game? Please select a number between 2 and 10.");
+            playString = menuPrompt.nextLine().toLowerCase();
+            try
+            {
+                players = Integer.parseInt(playString);
+            }
+            catch(NumberFormatException e)
+            {
+                System.out.println("Please type in a number between 2 and 10");
+            }
+            catch(Exception e)
+            {
+                System.out.println(e.getMessage() + " something went wrong.");
+            }
+        }
+        
+        while(!playString.equals("no"))
+        {
+            playString = "no";
+        }
     }
     
 }
