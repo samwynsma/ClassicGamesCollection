a59bc2447855eced005a6ddd567657175038cd46
samwynsma <cpcs.samuel.wynsma@gmail.com>
2026-07-06 14:36:24 -0700

    Added basic methods to poker hand.

--- GamesList/PokerMaterials/PokerHand.java
+++ GamesList/PokerMaterials/PokerHand.java
@@ -1,5 +1,44 @@
 package GamesList.PokerMaterials;
 
+import java.util.*;
+
+import GamesList.ToolsForMultipleGames.PokerHands;
+
 public class PokerHand {
-    
+    private List<String> cards;
+    private String defaultCards = "23456789TJQKA";
+    private int handValue;
+
+    public PokerHand()
+    {
+        cards = new ArrayList<String>();
+        handValue = 0;
+    }
+
+    public void Discard(List<Integer> discardLocs)
+    {
+        List<String> newCards = new ArrayList<String>();
+        for(int i = 0; i < cards.size(); i++)
+        {
+            if(!discardLocs.contains(i))
+                newCards.add(cards.get(i));
+        }
+        cards = newCards;
+    }
+
+    public int GetHandValue()
+    {
+        return handValue;
+    }
+
+    public void SetHandValue()
+    {
+        String[] cardSet = new String[cards.size()];
+        for(int i = 0; i < cardSet.length; i++)
+        {
+            cardSet[i] = cards.get(i);
+        }
+        int[] freq = PokerHands.GetCardFrequencies(defaultCards, cardSet);
+        handValue = 0;
+    }
 }
